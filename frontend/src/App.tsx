8bddebeb520c4192dc625973d839b62bcbae0b62
ZackAlaoui <alaoui.zacharia@gmail.com>
2024-11-15 22:58:05 +0000

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+import './App.css';
+
+import CardPage from './pages/CardPage.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="cards" element={<CardPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
