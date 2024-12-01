5811e0a5d5c6cb9f8afcc1df9c049a7ca05a32aa
ZachariaAlaoui <alaoui.zacharia@gmail.com>
2024-11-30 21:46:19 -0500

    trying to fix login screen

--- frontend/src/components/EditProfile.tsx
+++ frontend/src/components/EditProfile.tsx
@@ -6,49 +6,49 @@ function EditProfile() {
   return (
     <div className="containerEditProfile">
       <h2 id="editProfile">Edit Profile</h2>
-      <div className="Inputs">
-        FIRST NAME
-        <input type="text" placeholder="FIRST NAME" />
-      </div>
-      <br />
-      <div className="Inputs">
-        LAST NAME
-        <input type="text" placeholder="LAST NAME" />
-      </div>
-      <br />
-      <div className="Inputs">
-        USERNAME
-        <input type="text" placeholder="USERNAME" />
-      </div>
-      <br />
-      <div className="Inputs">
-        PASSWORD
-        <input type="text" placeholder="Change Password" />
-      </div>
-      <br />
-      <div className="Inputs">
-        GENDER
-        <input type="text" placeholder="Gender" />
-      </div>
-      <br />
-      <div className="Inputs">
-        <input type="text" placeholder="AGE" />
-      </div>
-      <br />
-      <div className="Inputs">
-        HEIGHT
-        <input type="text" placeholder="Height" />
-      </div>
-      <br />
-      <div className="Inputs">
-        WEIGHT
-        <input type="text" placeholder="Weight" />
-      </div>
-      <br />
-      <div className="Inputs">
-        EMAIL
-        <input type="text" placeholder="Email" />
-      </div>
+      {/* //   <div className="Inputs">
+    //     FIRST NAME
+    //     <input type="text" placeholder="FIRST NAME" />
+    //   </div>
+    //   <br />
+    //   <div className="Inputs">
+    //     LAST NAME
+    //     <input type="text" placeholder="LAST NAME" />
+    //   </div>
+    //   <br />
+    //   <div className="Inputs">
+    //     USERNAME
+    //     <input type="text" placeholder="USERNAME" />
+    //   </div>
+    //   <br />
+    //   <div className="Inputs">
+    //     PASSWORD
+    //     <input type="text" placeholder="Change Password" />
+    //   </div>
+    //   <br />
+    //   <div className="Inputs">
+    //     GENDER
+    //     <input type="text" placeholder="Gender" />
+    //   </div>
+    //   <br />
+    //   <div className="Inputs">
+    //     <input type="text" placeholder="AGE" />
+    //   </div>
+    //   <br />
+    //   <div className="Inputs">
+    //     HEIGHT
+    //     <input type="text" placeholder="Height" />
+    //   </div>
+    //   <br />
+    //   <div className="Inputs">
+    //     WEIGHT
+    //     <input type="text" placeholder="Weight" />
+    //   </div>
+    //   <br />
+    //   <div className="Inputs">
+    //     EMAIL
+    //     <input type="text" placeholder="Email" />
+    //   </div> */}
     </div>
   );
 }
