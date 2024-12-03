ddc5f6cc20d77616877eae6770d8cd8c771650ef
MattCarvajal <ma449997@ucf.edu>
2024-12-02 21:37:28 -0500

    debugging

--- server.js
+++ server.js
@@ -371,7 +371,7 @@ app.post('/v1/foods/search', async (req, res) => {
                 brandName: food.brandName || null,
                 calories: food.foodNutrients.find(n => n.nutrientName === 'Energy').value,
                 protein: food.foodNutrients.find(n => n.nutrientName === 'Protein').value,
-                foodId: foodItem.foodId,
+                foodId: food.foodNutrients.foodId,
             };
             enrichedResults.push(results);
         }
